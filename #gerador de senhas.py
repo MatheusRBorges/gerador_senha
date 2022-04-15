b47080901d6cfc8e5a852f16f01f099e98ad7c84
MXXB0RG3SXX <99483908+MXXB0RG3SXX@users.noreply.github.com>
2022-04-15 13:03:02 -0300

    Update #gerador de senhas.py

--- #gerador de senhas.py
+++ #gerador de senhas.py
@@ -9,7 +9,7 @@ symbols = "@#$^&*()!"
 
 for_pass = lower_case + upper_case + numbers + symbols
 
-size_password = 2
+size_password = 12
 
 password = "".join(random.sample(for_pass,size_password))
 
