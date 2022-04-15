fa1f901c36f4bc7cbc480f4f08a3acab5275e854
MXXB0RG3SXX <99483908+MXXB0RG3SXX@users.noreply.github.com>
2022-04-15 13:02:05 -0300

    Add files via upload

---
+++ #gerador de senhas.py
@@ -0,0 +1,16 @@
+#gerador de senhas 
+
+import random
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "@#$^&*()!"
+
+for_pass = lower_case + upper_case + numbers + symbols
+
+size_password = 2
+
+password = "".join(random.sample(for_pass,size_password))
+
+print("MINHA SENHA:",password)
